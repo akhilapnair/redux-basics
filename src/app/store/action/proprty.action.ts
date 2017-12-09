19cb54285c0c823e71ebcc42bf9b8f26244e8f8e
akhilapnair <akhi.prabha@gmail.com>
2017-12-09 10:46:31 +0530

    action and model integration

---
+++ src/app/store/action/proprty.action.ts
@@ -0,0 +1,21 @@
+// tslint:disable-next-line:eofline
+import { Action } from '@ngrx/store';
+import { Property } from '../../model/property.model';
+
+export const LOAD_PROPERTY = '[Products] load property';
+export const LOAD_PROPERTY_SUCESS = '[Products] load property sucess';
+export const LOAD_PROPERTY_FAIL = '[Products] load property fail';
+
+export class LoadProperty implements Action {
+readonly type = LOAD_PROPERTY;
+}
+
+export class LoadPropertyFail implements Action {
+    readonly type = LOAD_PROPERTY_FAIL;
+    constructor(public payload: any) {}
+}
+
+export class LoadPropertySucess implements Action {
+    readonly type = LOAD_PROPERTY_SUCESS;
+    constructor(public payload: any) {}
+}
